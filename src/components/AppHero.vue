805345acd4fa0ba755deb254d716f5941eb8b339
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-07 18:15:55 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            image: 'https://mdbcdn.b-cdn.net/img/new/slides/041.webp',
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <img :src="image">
+    </div>
+</template>
+
+<style scoped>
+div {
+    width: 100%;
+    height: 100%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!',
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1 class="red">{{ title }}</h1>
+</template>
+
+<style scoped>
+.red {
+    color: red;
+}
+</style>
